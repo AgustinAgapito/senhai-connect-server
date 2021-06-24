5c9181bae8c3b5764e231a0bece7c42e10a60adf
SenpaiKouhai <76901954+SenpaiKouhai@users.noreply.github.com>
2021-06-24 08:13:14 +0800

    cors enable
    the only problem was the typo on link

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var port = process.env.PORT || 4000;
 const server = http.createServer(app);
 const io = socket(server, { 
     cors: { 
-        origin: 'https://sh-connect.vercel.app' ,
+        origin: '*' ,
         methods: ["GET", "POST"],
     },
     // maxHttpBufferSize: 80e8,
@@ -24,19 +24,19 @@ const io = socket(server, {
 //1e8
 //80e8
 
-var whitelist = ['http://localhost:3000', 'https://sh-connect.vercel.app', 'https://sh-drama.netlify.app']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// var whitelist = ['http://localhost:3000', 'https://sh-connect.vercel.app', 'https://sh-drama.netlify.app']
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
 
-app.use(cors(corsOptionsDelegate))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(__dirname));
